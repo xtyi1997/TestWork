9e0bfd8100a1734256863f2b8c4b7a6b1932d5e3
yixintong <yixintong@huice.com>
2023-05-30 10:40:04 +0800

    init

---
+++ src/main/java/com/example/testworkdemo/TestWorkDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.testworkdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWorkDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestWorkDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testworkdemo/TestWorkDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testworkdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestWorkDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
