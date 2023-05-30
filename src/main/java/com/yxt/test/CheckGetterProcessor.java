0bf8e3849aad0d23c5641875da3617be353d4054
yixintong <yixintong@huice.com>
2023-05-30 16:56:12 +0800

    ceshi--1

---
+++ src/main/java/com/yxt/test/CheckGetterProcessor.java
@@ -0,0 +1,54 @@
+package com.yxt.test;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.VariableElement;
+import javax.lang.model.util.ElementFilter;
+import javax.tools.Diagnostic;
+import javax.lang.model.element.Element;
+import java.util.Set;
+
+/**
+ * @version V1.0
+ * @author: xintong yi
+ * @date: 2023年05月30日 16时53分
+ * @contact yixintong@huice.com
+ * @company 掌上先机 (http://www.huice.com)
+ */
+@SupportedAnnotationTypes("com.yxt.test.CheckGetter")
+@SupportedSourceVersion(SourceVersion.RELEASE_11)
+public class CheckGetterProcessor extends AbstractProcessor {
+
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        for (TypeElement annotatedClass : ElementFilter.typesIn(roundEnv.getElementsAnnotatedWith(CheckGetter.class))) {
+            for (VariableElement field : ElementFilter.fieldsIn(annotatedClass.getEnclosedElements())) {
+                if(!containsGetter(annotatedClass, field.getSimpleName().toString())){
+                    processingEnv.getMessager().printMessage(Diagnostic.Kind.ERROR,
+                            String.format("getter not found for '%s.%s'.", annotatedClass.getSimpleName(), field.getSimpleName()));
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean containsGetter(TypeElement typeElement, String name){
+        String getter = "get" + name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+        for(ExecutableElement executableElement: ElementFilter.methodsIn(typeElement.getEnclosedElements())){
+            if(!executableElement.getModifiers().contains(Modifier.STATIC) &&
+                    executableElement.getSimpleName().toString().equals(getter) &&
+                    executableElement.getParameters().isEmpty()){
+                return true;
+            }
+
+        }
+        return false;
+    }
+}
