93ae0be409e70876efd8558e3a06c48e34f89ee4
yixintong <yixintong@huice.com>
2023-06-02 16:30:24 +0800

    ceshi--1

---
+++ src/main/java/com/yxt/test/ceshiGithub.java
@@ -0,0 +1,14 @@
+package com.yxt.test;
+
+/**
+ * @version V1.0
+ * @author: xintong yi
+ * @date: 2023年06月02日 16时30分
+ * @contact yixintong@huice.com
+ * @company 掌上先机 (http://www.huice.com)
+ */
+public class ceshiGithub {
+    public static void main(String[] args) {
+        System.out.println("123123");
+    }
+}
