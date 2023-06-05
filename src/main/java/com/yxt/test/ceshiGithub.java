a2012224f58cc17381c6622ca7ea2756f6404298
yixintong <yixintong@huice.com>
2023-06-05 09:20:28 +0800

    ceshi--2

--- src/main/java/com/yxt/test/ceshiGithub.java
+++ src/main/java/com/yxt/test/ceshiGithub.java
@@ -10,5 +10,6 @@ package com.yxt.test;
 public class ceshiGithub {
     public static void main(String[] args) {
         System.out.println("123123");
+        System.out.println("432423");
     }
 }
