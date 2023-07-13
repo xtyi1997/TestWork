4db842cb7e1ac7f756b412e69e9ee517465e35f1
yixintong <2587085988@qq.com>
2023-07-13 09:23:17 +0800

    ceshi--4

---
+++ src/main/java/com/yxt/test/ceshi_1.java
@@ -0,0 +1,15 @@
+package com.yxt.test;
+
+/**
+ * @version V1.0
+ * @author: xintong yi
+ * @date: 2023年07月01日 12时04分
+ * @contact yixintong@huice.com
+ * @company 掌上先机 (http://www.huice.com)
+ */
+public class ceshi_1 {
+    public static void main(String[] args) {
+        String abc="";
+        System.out.println(abc.length());
+    }
+}
---
+++ src/main/java/com/yxt/test/testCanshu.java
@@ -0,0 +1,11 @@
+package com.yxt.test;
+
+/**
+ * @version V1.0
+ * @author: xintong yi
+ * @date: 2023年07月13日 09时23分
+ * @contact yixintong@huice.com
+ * @company 掌上先机 (http://www.huice.com)
+ */
+public class testCanshu {
+}
